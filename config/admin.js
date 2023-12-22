446ef685341bd929c58fabd0c4f688b7b1ee5b50
eduvlemes <edu.vlemes@gmail.com>
2023-12-21 21:16:48 -0300

    p

--- config/admin.js
+++ config/admin.js
@@ -1,9 +1,14 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'ALPIX'),
+    secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT','ALPIX'),
+    salt: env('API_TOKEN_SALT'),
+  },
+  transfer: {
+    token: {
+      salt:  env('API_TOKEN_SALT'),
+    },
   },
 
 });
